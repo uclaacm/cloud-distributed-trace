ddb043c020bb10e137efa69bf48c83c2a97e4636
siphc <ayfpan@ucla.edu>
2025-02-08 16:36:29 -0800

    implement ts-aggregate

--- include/ts-aggregate.h
+++ include/ts-aggregate.h
@@ -6,6 +6,9 @@
 struct logentry {
     int timestamp;
     std::string content;
+
+    friend bool operator<(const logentry& lhs, const logentry& rhs);
+    friend std::ostream& operator<<(std::ostream& stream, const logentry& output);
 };
 
-void tsaggregate(std::vector<std::string> filepaths[]);
+void tsaggregate(const std::vector<std::string>& filepaths);
--- lib/ts-aggregate.cpp
+++ lib/ts-aggregate.cpp
@@ -1,5 +1,52 @@
-#include "ts-aggregate.h"
+#include "../include/ts-aggregate.h"
+#include <vector>
+#include <string>
+#include <fstream>
+#include <algorithm>
 
-void tsaggregate(std::vector<std::string> filepaths[]) {
-    
+
+bool operator<(const logentry& lhs, const logentry& rhs) {
+    if (lhs.timestamp < rhs.timestamp) return true;
+    else return false;
 }
+
+std::ostream& operator<<(std::ostream& stream, const logentry& output) {
+    stream << output.timestamp << ' ' << output.content;
+    return stream;
+}
+
+void tsaggregate(const std::vector<std::string>& filepaths) {
+    std::vector<logentry> logs;
+
+    for (const std::string& filename : filepaths) {
+        std::ifstream input(filename);
+
+        while (input.peek()!=-1) {
+            std::string timestamp;
+            input >> timestamp;
+            
+            char message[100]; // this is inefficient code!
+            input.getline(message, 100);
+
+            logentry curr;
+            curr.timestamp = std::stoi(timestamp);
+            curr.content = std::string(message);
+
+            logs.push_back(curr);
+        }
+
+        input.close();
+    }
+
+    std::sort(logs.begin(), logs.end());
+
+    std::ofstream output("../log/aggregated.log");
+    for (const logentry& entry : logs) {
+        output << entry << std::endl;
+    }
+    output.close();
+}
+
+// int main() {
+//     tsaggregate({"../log/1.log", "../log/2.log"});
+// }
