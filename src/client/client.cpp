be39fb2564c677a40f1429d9333d210c9cf0aa01
Daniel Yang <danielyangkang@gmail.com>
2025-02-12 21:36:20 -0800

    Working POSIX socket example

--- src/client/client.cpp
+++ src/client/client.cpp
@@ -1,6 +1,49 @@
-#include "distributed_trace.h"
 #include <iostream>
+#include <string>
+#include <sys/socket.h>
+#include <arpa/inet.h>
+#include <unistd.h>
+
+#define PORT 8080
 
 int main() {
-    std::cout << "THIS IS THE CLIENT" << std::endl;
+    int sock = 0;
+    struct sockaddr_in serv_addr;
+    char buffer[1024] = {0};
+    
+    // Create socket
+    if ((sock = socket(AF_INET, SOCK_STREAM, 0)) < 0) {
+        perror("Socket creation failed");
+        return -1;
+    }
+
+    serv_addr.sin_family = AF_INET;
+    serv_addr.sin_port = htons(PORT);
+
+    // Convert IP address to binary form
+    if (inet_pton(AF_INET, "192.168.0.100", &serv_addr.sin_addr) <= 0) {
+        perror("Invalid address");
+        return -1;
+    }
+
+    // Connect to the server
+    if (connect(sock, (struct sockaddr*)&serv_addr, sizeof(serv_addr)) < 0) {
+        perror("Connection failed");
+        return -1;
+    }
+
+    // Send a message to the server
+    std::string message = "Hello from client!";
+    send(sock, message.c_str(), message.length(), 0);
+    std::cout << "Message sent" << std::endl;
+
+    // Read server's response
+    read(sock, buffer, 1024);
+    std::cout << "Server response: " << buffer << std::endl;
+
+    // Close the socket
+    close(sock);
+
+    return 0;
 }
+
